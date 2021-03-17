5f91c82b7faef4c8b65edb822355fe27c5fb582c
raphael-oliveira1989 <79456233+raphael-oliveira1989@users.noreply.github.com>
2021-03-17 11:44:45 -0300

    Add files via upload

--- 2 - Classes/3/PackApp.java
+++ 2 - Classes/3/PackApp.java
@@ -14,9 +14,8 @@ class PackApp {
 class PackFrame extends JFrame {
     Rect r1;
     Ellipse e1,e2;
-	Arc a1;
-	Arc a2;
-	Arc a3;
+	Arc a1,a2,a3;
+	Triang t1;
 
     PackFrame () {
         this.addWindowListener (
@@ -28,12 +27,17 @@ class PackFrame extends JFrame {
         );
         this.setTitle("Java Packages");
         this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30,0,255,0,166,166,166);
-        this.e1 = new Ellipse(50,150, 100,30,255,0,0,0,255,255);
-		this.a1 = new Arc(200,100,50,50,0,270,0,0,0);
-		this.a2 = new Arc(200,200,80,80,30,300,255,255,0);
-		this.a3 = new Arc(50,220,50,50,90,180,0,255,0);
-		this.e2 = new Ellipse(230,215, 15,15,0,0,0,0,0,0);
+		//Rect (int x, int y, int w, int h, Color bd, Color bg)
+        this.r1 = new Rect(50,50, 100,30,Color.red,Color.gray);
+		//Ellipse (int x, int y, int w, int h, Color bd, Color bg)
+        this.e1 = new Ellipse(50,150, 100,30,Color.green,Color.magenta);
+		//Arc (int x, int y, int w, int h, int arci, int arcf, Color bg)
+		this.a1 = new Arc(200,100,50,50,0,270,Color.blue);
+		this.a2 = new Arc(200,200,80,80,30,300,Color.yellow);
+		this.a3 = new Arc(50,220,100,100,90,135,Color.cyan);
+		this.e2 = new Ellipse(230,215, 15,15,Color.yellow,Color.black);
+		//Triang (int x1,int x2,int x3,int y1,int y2,int y3, Color bd, Color bg)
+		this.t1 = new Triang(100,130,200,250,200,300,Color.orange);
     }
 
     public void paint (Graphics g) {
@@ -44,5 +48,6 @@ class PackFrame extends JFrame {
 		this.a2.paint_p(g);
 		this.a3.paint_c(g);
 		this.e2.paint(g);
+		this.t1.paint(g);
 	}
 }
