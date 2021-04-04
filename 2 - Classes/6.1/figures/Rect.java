3d17f9e6110faf9baa7d22f097e44cd66839b40a
raphael-oliveira1989 <79456233+raphael-oliveira1989@users.noreply.github.com>
2021-04-04 16:14:45 -0300

    Add files via upload

---
+++ 2 - Classes/6.1/figures/Rect.java
@@ -0,0 +1,60 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure{
+    private int x, y;
+    private int w, h;
+	private Color bg, bd;
+
+    public Rect (int x, int y, int w, int h, Color bd, Color bg) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+		this.bd = bd;
+		this.bg = bg;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(bd);
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+		/*
+		this.i = x+1;
+		this.j = y+1;
+		this.l = w-1;
+		this.a = h-1;
+		*/
+		g2d.setColor(bg);
+		g2d.fillRect(this.x+1,this.y+1, this.w-1,this.h-1);
+    }
+}
+/*
+public class Rect {
+    int x, y;
+    int w, h;
+
+    public Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
+*/
