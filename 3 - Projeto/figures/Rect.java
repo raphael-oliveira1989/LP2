0f45937bf68f8ebf406187cb9e9e80f7fa2ab781
raphael-oliveira1989 <79456233+raphael-oliveira1989@users.noreply.github.com>
2021-04-24 19:18:44 -0300

    Add files via upload

--- 3 - Projeto/figures/Rect.java
+++ 3 - Projeto/figures/Rect.java
@@ -7,6 +7,13 @@ public class Rect extends Figure{
     public Rect (int x, int y, int w, int h, Color bd, Color bg,char c) {
         super(x,y,w,h,bd,bg,c);
     }
+	
+	public void set(int x, int y, int w, int h){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
 
     public void print () {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n", w, h, x, y);
