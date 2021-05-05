f3b00173251e4aad0b7d034121c6eea1f1a70d28
raphael-oliveira1989 <79456233+raphael-oliveira1989@users.noreply.github.com>
2021-05-05 11:04:42 -0300

    Add files via upload

---
+++ 4 - Interfaces/figures/Ellipse.java
@@ -0,0 +1,23 @@
+package figures;
+
+import java.awt.*;
+
+public class Ellipse extends Figure{
+
+    public Ellipse (int x, int y, int w, int h, Color bd, Color bg,char c) {
+		super(x,y,w,h,bd,bg,c);
+    }
+
+    public void print () {
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, x, y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(bd);
+        g2d.drawOval(x,y, this.w,this.h);
+		g2d.setColor(bg);
+		g2d.fillOval(x+1,y+1, this.w-1,this.h-1);
+    }
+}
